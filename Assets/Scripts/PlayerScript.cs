9770a4828974b28834a002cb3a8637173eb1aa56
ultra-v1olence <sechkarevs@gmail.com>
2020-06-03 21:14:48 +0300

    use delta time to control the stick

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,14 +6,14 @@ public class PlayerScript : MonoBehaviour
     public ReviewScript reviewLogic;
     
     private const float VerticalBoundary = 0.85f;
-    private const float AppliedVelocity = 0.05f;
+    private const float AppliedVelocity = 3.0f;
     
     private Vector3 _currentVelocity = Vector3.zero;
     public void OnMove(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
         {
-            _currentVelocity = context.ReadValue<Vector2>();
+            _currentVelocity = context.ReadValue<Vector2>() * AppliedVelocity;
         }
         else
         {
@@ -29,13 +29,13 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         float currentY = transform.position.y;
         if ((currentY > -VerticalBoundary && _currentVelocity.y < 0) 
             || (currentY < VerticalBoundary && _currentVelocity.y > 0))
         {
-            transform.position += _currentVelocity * AppliedVelocity;
+            transform.position += _currentVelocity * Time.deltaTime;
         }
     }
 }
