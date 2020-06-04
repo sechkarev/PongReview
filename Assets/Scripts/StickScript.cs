aa9d7fb7122b18fc088f68a44a158cab2519ceba
ultra-v1olence <sechkarevs@gmail.com>
2020-06-04 21:08:40 +0300

    make player collider the whole stick

--- Assets/Scripts/StickScript.cs
+++ Assets/Scripts/StickScript.cs
@@ -6,10 +6,9 @@ public class StickScript : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Rigidbody2D otherObjectRigidbody = other.rigidbody;
-        if (otherObjectRigidbody != null)
+        if (other.gameObject.tag.Equals("Ball"))
         {
-            otherObjectRigidbody.velocity *= new Vector2(-1, 1);
+            other.rigidbody.velocity *= new Vector2(-1, 1);
         }
     }
 }
